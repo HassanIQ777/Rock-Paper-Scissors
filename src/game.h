580244e69dd1baf0b121e1ef59e5ca9f000f1a58
HassanIQ777 <hassaniq793@gmail.com>
2025-09-01 01:48:11 +0300

    Added enum States and function char* stateToString

--- src/game.h
+++ src/game.h
@@ -1,2 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
+
+enum States {
+    ROCK,
+    PAPER,
+    SCISSORS
+};
+
+char* stateToString(int State){
+    char* ret; // return
+
+    switch(State) {
+        case ROCK:
+			ret = "Rock";
+        	break;
+
+        case PAPER:
+			ret = "Paper";
+			break;
+
+        case SCISSORS:
+			ret = "Scissors";
+	        break;
+    } // no need for default
+
+    return ret;
+}
--- src/main.c
+++ src/main.c
@@ -1,8 +1,5 @@
 /* Rock-Paper-Scissors by HassanIQ777 & حط اسمك*/
-
 #include "game.h"
 
-int main(int argc, char* argv[])
-{
-
-}
+int main(/*int argc, char* argv[]*/) // not needed (yet)
+{}
